2ca3389103227264786e467839b93576ca215d9e
Rene0115 <enere0115@gmail.com>
2023-02-19 17:08:53 +0100

    added create table route

--- routers/user.routes.js
+++ routers/user.routes.js
@@ -1,5 +1,9 @@
+/* eslint-disable import/extensions */
 import express from 'express';
+import userController from '../controllers/user.controller.js';
 
 const userRouter = express.Router();
+
+userRouter.post('/createtable', userController.create);
 
 export default userRouter;
